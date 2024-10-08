611c039bbc87b63446453c8bfcebee2019377bdd
Angel <angeldevvvv@gmail.com>
2024-10-08 23:07:56 +0200

    fixed the styling of the logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
                         <FontAwesomeIcon icon={faUserCircle}/>
                         {user && user.firstName}
                     </Link>
-                    <button onClick={handleLogout} className={styles.navItem}>
+                    <button onClick={handleLogout} className={styles.navButton}>
                         <FontAwesomeIcon icon={faSignOut}/>
                         Sign Out
                     </button>
